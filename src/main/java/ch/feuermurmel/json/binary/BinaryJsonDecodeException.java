7b503c4609f15ce1f8fe4e06adcdd01784785d35
Michael Schwarz <michi.schwarz@gmail.com>
2013-12-08 11:55:42 +0100

    Removed unfinished binary serialization API. It will be continued on a separate branch.

--- src/main/java/ch/feuermurmel/json/binary/BinaryJsonDecodeException.java
+++
@@ -1,9 +0,0 @@
-package ch.feuermurmel.json.binary;
-
-import java.io.IOException;
-
-public class BinaryJsonDecodeException extends IOException {
-	public BinaryJsonDecodeException(String msg) {
-		super(msg);
-	}
-}
